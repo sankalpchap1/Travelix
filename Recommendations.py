f6b8d8b757b39bd46c73b1591229f04cab791081
sankalpc7 <alexStraut@7798>
2023-04-27 03:15:56 -0500

    integrating the backend in api, creating NPR API

---
+++ Recommendations.py
@@ -0,0 +1,110 @@
+# Building Ratings Matrix
+from scipy.sparse import coo_matrix
+import numpy as np
+from collections import defaultdict
+from Business import Business
+
+
+class Recommendations:
+    def __init__(self, business_df, reviews_df):
+        self.business_df = business_df
+        self.reviews_df = reviews_df
+        self.ratings_mat = []
+        self.user_num_to_user_hash_dict = dict()
+        self.user_hash_to_user_num_dict = dict()
+        self.business_num_to_business_hash_dict = dict()
+        self.business_hash_to_business_num_dict = dict()
+        self.business_recommendations = []
+        self.business_popularity = []
+        self.calculateRatingMatrix()
+        self.nonPersonalizedRecommendations()
+
+    def calculateRatingMatrix(self):
+        print("Calculating rating matrix...")
+        business_list = list(self.business_df['business_id'])
+        reviews_df_updated = self.reviews_df[self.reviews_df['business_id'].isin(
+            business_list)]
+
+        unique_business_id = reviews_df_updated['business_id'].unique()
+        unique_user_id = reviews_df_updated['user_id'].unique()
+
+        j = 0
+        for u in unique_user_id:
+            self.user_hash_to_user_num_dict[u] = j
+            self.user_num_to_user_hash_dict[j] = u
+            j += 1
+
+        j = 0
+        for i in unique_business_id:
+            self.business_hash_to_business_num_dict[i] = j
+            self.business_num_to_business_hash_dict[j] = i
+            j += 1
+
+        # Then, use the generated dictionaries to reindex UserID and MovieID in the data_df
+        user_list = reviews_df_updated['user_id'].values
+        movie_list = reviews_df_updated['business_id'].values
+        for j in range(len(reviews_df_updated)):
+            user_list[j] = self.user_hash_to_user_num_dict[user_list[j]]
+            movie_list[j] = self.business_hash_to_business_num_dict[movie_list[j]]
+        reviews_df_updated['user_id'] = user_list
+        reviews_df_updated['business_id'] = movie_list
+
+        num_user = len(reviews_df_updated['user_id'].unique())
+        num_movie = len(reviews_df_updated['business_id'].unique())
+
+        self.ratings_mat = coo_matrix((reviews_df_updated['stars'].values, (reviews_df_updated['user_id'].values,
+                                      reviews_df_updated['business_id'].values)), shape=(num_user, num_movie)).astype(float).toarray()
+
+    def nonPersonalizedRecommendations(self):
+        print("Calculating NPR...")
+        n = len(self.ratings_mat)  # number of users
+        m = len(self.ratings_mat[0])  # number of movies
+
+        # Creating popularity array - size number of movies
+        self.business_popularity = np.zeros((m,))
+        # claculating the popularity of each movie by summing the values in each column
+        self.business_popularity = self.ratings_mat.sum(axis=0)
+
+        self.business_recommendations = np.zeros((n, 50), dtype=np.int32)
+
+        for u in range(self.ratings_mat.shape[0]):
+            business_unvisited = np.where(self.ratings_mat[u] == 0)[0]
+            unwatched_popularity = self.business_popularity[business_unvisited]
+            # Sort the unwatched movies according to popularity and fetch top 50 to recommend
+            self.business_recommendations[u] = business_unvisited[np.argsort(
+                unwatched_popularity)[::-1]][:50]
+
+        # print("Non personalized recommendations for first User:")
+        # for i in range(5):
+        #   business_hash = self.getBusinessHashFromBusinessNum(self.business_recommendations[0,i])
+        #   business = self.getBusinessInfo(business_hash)
+        #   print(f"Rank {i+1}: Business {self.business_recommendations[0,i]} - Name: {business.name} - state: {business.state} - stars: {business.stars}  - Popularity {business_popularity[self.business_recommendations[0,i]]}")
+
+    def getNPRForuUser(self, user_num):
+        print(f"Non personalized recommendations for User {user_num}:")
+        business_list = []
+        for i in range(5):
+            business_hash = self.getBusinessHashFromBusinessNum(
+                self.business_recommendations[0, i])
+            business = self.getBusinessInfo(business_hash)
+            business_list.append(business)
+            print(
+                f"Rank {i+1}: Business {self.business_recommendations[0,i]} - Name: {business.name} - state: {business.state} - stars: {business.stars}  - Popularity {self.business_popularity[self.business_recommendations[0,i]]}")
+        return business_list
+
+    def getUserHashFromUserNum(self, user_num):
+        return self.user_num_to_user_hash_dict[user_num]
+
+    def getUserNumFromUserHash(self, user_hash):
+        return self.user_hash_to_user_num_dict[user_hash]
+
+    def getBusinessHashFromBusinessNum(self, business_num):
+        return self.business_num_to_business_hash_dict[business_num]
+
+    def getBusinessNumFromBusinessHash(self, business_hash):
+        return self.business_hash_to_business_num_dict[business_hash]
+
+    def getBusinessInfo(self, business_hash):
+        bus_df = self.business_df[self.business_df['business_id']
+                                  == business_hash].iloc[0]
+        return Business(bus_df['name'], bus_df['address'], bus_df['city'], bus_df['state'], bus_df['postal_code'], bus_df['stars'])
--- app.py
+++ app.py
@@ -1,25 +1,153 @@
+from collections import defaultdict
+from scipy.sparse import coo_matrix
 from flask import Flask, send_from_directory
-from flask_cors import CORS,cross_origin
+from flask_cors import CORS, cross_origin
+import os
+import json
+import re
+import sys
+import csv
+import pandas as pd
+import numpy as np
+import seaborn as seab
+import pickle
+import datetime
+from Recommendations import Recommendations
+from Business import Business
+import bz2file as bz2
+import json
+# print("pickle version"+pickle.format_version)
 
 
-app = Flask(__name__,static_folder='./build',static_url_path="")
+app = Flask(__name__, static_folder='./build', static_url_path="")
 CORS(app)
+
+business_df = pd.DataFrame()
+reviews_df = pd.DataFrame()
+# create an empty dictionary to store the dataframes
+hotel_state_df_map = {}
+restaurent_state_df_map = {}
+
 
 @app.route('/profile')
 @cross_origin()
 def my_profile():
     response_body = {
         "name": "Nagato",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
+        "about": "Hello! I'm a full stack developer that loves python and javascript"
     }
     return response_body
+
 
 @app.route('/')
 @cross_origin()
 def serve():
-    return send_from_directory(app.static_folder,'index.html')
+    return send_from_directory(app.static_folder, 'index.html')
+
+
+def loadCSV():
+    print("Loading CSV Files...")
+    print(datetime.datetime.now())
+    global reviews_df
+    reviews_df = pd.read_csv('yelp_academic_dataset_review.csv')
+    # user_df = pd.read_csv('yelp_academic_dataset_user.csv')
+    global business_df
+    business_df = pd.read_csv('yelp_academic_dataset_business.csv')
+    business_df = business_df.dropna(subset=['categories'])
+    print("Loading CSV Files Completed...")
+    print(datetime.datetime.now())
+    print("\n")
+
+
+def getTopStates():
+    # Get Top Ten States
+    print("Generating Top 10 States Dataframes...")
+    unique_states = business_df['state'].unique()
+    state_map = dict()
+    for s in unique_states:
+        state_map[s] = business_df[business_df['state'] == s].shape[0]
+    # 'CA' 'MO' 'AZ' 'PA' 'TN' 'FL' 'IN' 'LA' 'AB' 'NV' 'ID' 'DE' 'IL' 'NJ' 'NC' 'CO' 'WA' 'HI' 'UT' 'TX' 'MT' 'MI' 'SD' 'XMS' 'MA' 'VI' 'VT'
+    top_states = [state[0] for state in sorted(sorted(state_map.items(
+    ), key=lambda x: x[1], reverse=True), key=lambda x: x[1], reverse=True)[:10]]
+    print(top_states)
+
+    # Creating mask for Hotels & Travel
+    hotel_mask = business_df['categories'].str.contains('Hotels & Travel')
+    hotel_df = business_df[hotel_mask]
+
+    # Creating mask for Restaurents
+    restaurent_mask = business_df['categories'].str.contains('Restaurants')
+    restaurent_df = business_df[restaurent_mask]
+
+    global hotel_state_df_map
+    global restaurent_state_df_map
+
+    for state in top_states:
+        df_name = f'business_df_{state}'
+
+        hotel_state_df = hotel_df[hotel_df['state'] == state]
+        restaurent_state_df = restaurent_df[restaurent_df['state'] == state]
+
+        exec(f"{df_name} = hotel_state_df")
+        # add the dataframe to the dictionary with the state abbreviation as the key
+        hotel_state_df_map[state] = hotel_state_df
+
+        exec(f"{df_name} = restaurent_state_df")
+        # add the dataframe to the dictionary with the state abbreviation as the key
+        restaurent_state_df_map[state] = restaurent_state_df
+
+
+def compressed_pickle(title, data):
+    with bz2.BZ2File(title + ".pbz2", "w") as f:
+        pickle.dump(data, f)
+
+
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, "rb")
+    data = pickle.load(data)
+    return data
+
+
+# loadCSV()
+# getTopStates()
+hotel_state_rec_map = {}
+print("Loading Data Files...")
+print(datetime.datetime.now())
+PA_Hotel_Recommendation = decompress_pickle("PA_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['PA'] = PA_Hotel_Recommendation
+FL_Hotel_Recommendation = decompress_pickle("FL_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['FL'] = FL_Hotel_Recommendation
+TN_Hotel_Recommendation = decompress_pickle("TN_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['TN'] = TN_Hotel_Recommendation
+IN_Hotel_Recommendation = decompress_pickle("IN_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['IN'] = IN_Hotel_Recommendation
+MO_Hotel_Recommendation = decompress_pickle("MO_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['MO'] = MO_Hotel_Recommendation
+LA_Hotel_Recommendation = decompress_pickle("LA_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['LA'] = LA_Hotel_Recommendation
+AZ_Hotel_Recommendation = decompress_pickle("AZ_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['AZ'] = AZ_Hotel_Recommendation
+NJ_Hotel_Recommendation = decompress_pickle("NJ_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['NJ'] = NJ_Hotel_Recommendation
+NV_Hotel_Recommendation = decompress_pickle("NV_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['NV'] = NV_Hotel_Recommendation
+AB_Hotel_Recommendation = decompress_pickle("AB_Hotel_Recommendation.pbz2")
+hotel_state_rec_map['AB'] = AB_Hotel_Recommendation
+print("Loading Data Files Completed...")
+print(datetime.datetime.now())
+print("\n")
+
+
+@app.route('/<string:state_name>/<int:user_id>/getNPR')
+@cross_origin()
+def getRecommendation(state_name, user_id):
+    global hotel_state_rec_map
+    business_list = hotel_state_rec_map[state_name].getNPRForuUser(user_id)
+    response = json.dumps(
+        [{'name': business.name, 'address': business.address, 'city': business.city, 'state': business.state, 'postal_code': business.postal_code, 'stars': business.stars} for business in business_list])
+    print(response)
+    return response
+
 
 if __name__ == '__main__':
-    app.debug = True
-    print("HELLO FROM APP.PY")
-    app.run()
+    app.run(host='0.0.0.0', port=8080, debug=False)
