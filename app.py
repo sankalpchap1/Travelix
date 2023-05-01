782df14773985ea05fca44a7bbdf8d6e2efd3ad0
Sankalp Chapalgaonkar <sankalpchap1@gmail.com>
2023-04-30 19:27:53 -0500

    changes for app.app_context

--- app.py
+++ app.py
@@ -160,24 +160,98 @@ restaurent_aecf_map = {}
 
 nightlife_aecf_map = {}
 
+
+def loadCSV():
+    print("Loading CSV Files...")
+    print(datetime.datetime.now())
+    global reviews_df
+    reviews_df = pd.read_csv('yelp_academic_dataset_review.csv')
+    # user_df = pd.read_csv('yelp_academic_dataset_user.csv')
+    global business_df
+    business_df = pd.read_csv('yelp_academic_dataset_business.csv')
+    business_df = business_df.dropna(subset=['categories'])
+    print("Loading CSV Files Completed...")
+    print(datetime.datetime.now())
+    print("\n")
+
+
+def getTopStates():
+    # Get Top Ten States
+    print("Generating Top 10 States Dataframes...")
+    unique_states = business_df['state'].unique()
+    state_map = dict()
+    for s in unique_states:
+        state_map[s] = business_df[business_df['state'] == s].shape[0]
+    # 'CA' 'MO' 'AZ' 'PA' 'TN' 'FL' 'IN' 'LA' 'AB' 'NV' 'ID' 'DE' 'IL' 'NJ' 'NC' 'CO' 'WA' 'HI' 'UT' 'TX' 'MT' 'MI' 'SD' 'XMS' 'MA' 'VI' 'VT'
+    top_states = [state[0] for state in sorted(sorted(state_map.items(
+    ), key=lambda x: x[1], reverse=True), key=lambda x: x[1], reverse=True)[:10]]
+    print(top_states)
+
+    # Creating mask for Hotels & Travel
+    hotel_mask = business_df['categories'].str.contains('Hotels & Travel')
+    hotel_df = business_df[hotel_mask]
+
+    # Creating mask for Restaurents
+    restaurent_mask = business_df['categories'].str.contains('Restaurants')
+    restaurent_df = business_df[restaurent_mask]
+
+    # Creating mask for Nightlife
+    nightlife_mask = business_df['categories'].str.contains('Nightlife')
+    nightlife_df = business_df[nightlife_mask]
+
+    global hotel_state_df_map
+    global restaurent_state_df_map
+    global nightlife_state_df_map
+
+    for state in top_states:
+        df_name = f'business_df_{state}'
+
+        hotel_state_df = hotel_df[hotel_df['state'] == state]
+        restaurent_state_df = restaurent_df[restaurent_df['state'] == state]
+        nightlife_state_df = nightlife_df[nightlife_df['state'] == state]
+
+        exec(f"{df_name} = hotel_state_df")
+        # add the dataframe to the dictionary with the state abbreviation as the key
+        hotel_state_df_map[state] = hotel_state_df
+
+        exec(f"{df_name} = restaurent_state_df")
+        # add the dataframe to the dictionary with the state abbreviation as the key
+        restaurent_state_df_map[state] = restaurent_state_df
+
+        exec(f"{df_name} = nightlife_state_df")
+        # add the dataframe to the dictionary with the state abbreviation as the key
+        nightlife_state_df_map[state] = nightlife_state_df
+
+
+def compressed_pickle(title, data):
+    with bz2.BZ2File(title + ".pbz2", "w") as f:
+        pickle.dump(data, f)
+
+
+def decompress_pickle(file):
+    data = bz2.BZ2File(file, "rb")
+    data = pickle.load(data)
+    return data
+
+
 def helper_fun():
-    global hotel_state_rec_map
-    
-    global restaurent_state_rec_map
-    
-    global nightlife_state_rec_map
-    
-    global hotel_mf_map
-    
-    global restaurent_mf_map
-    
-    global nightlife_mf_map
-    
-    global hotel_aecf_map
-    
-    global restaurent_aecf_map
-    
-    global nightlife_aecf_map
+    # global hotel_state_rec_map
+
+    # global restaurent_state_rec_map
+
+    # global nightlife_state_rec_map
+
+    # global hotel_mf_map
+
+    # global restaurent_mf_map
+
+    # global nightlife_mf_map
+
+    # global hotel_aecf_map
+
+    # global restaurent_aecf_map
+
+    # global nightlife_aecf_map
 
     print("Loading Hotel Data Files...")
     print(datetime.datetime.now())
@@ -493,78 +567,9 @@ def helper_fun():
     print(datetime.datetime.now())
     print("\n")
 
-def loadCSV():
-    print("Loading CSV Files...")
-    print(datetime.datetime.now())
-    global reviews_df
-    reviews_df = pd.read_csv('yelp_academic_dataset_review.csv')
-    # user_df = pd.read_csv('yelp_academic_dataset_user.csv')
-    global business_df
-    business_df = pd.read_csv('yelp_academic_dataset_business.csv')
-    business_df = business_df.dropna(subset=['categories'])
-    print("Loading CSV Files Completed...")
-    print(datetime.datetime.now())
-    print("\n")
 
-
-def getTopStates():
-    # Get Top Ten States
-    print("Generating Top 10 States Dataframes...")
-    unique_states = business_df['state'].unique()
-    state_map = dict()
-    for s in unique_states:
-        state_map[s] = business_df[business_df['state'] == s].shape[0]
-    # 'CA' 'MO' 'AZ' 'PA' 'TN' 'FL' 'IN' 'LA' 'AB' 'NV' 'ID' 'DE' 'IL' 'NJ' 'NC' 'CO' 'WA' 'HI' 'UT' 'TX' 'MT' 'MI' 'SD' 'XMS' 'MA' 'VI' 'VT'
-    top_states = [state[0] for state in sorted(sorted(state_map.items(
-    ), key=lambda x: x[1], reverse=True), key=lambda x: x[1], reverse=True)[:10]]
-    print(top_states)
-
-    # Creating mask for Hotels & Travel
-    hotel_mask = business_df['categories'].str.contains('Hotels & Travel')
-    hotel_df = business_df[hotel_mask]
-
-    # Creating mask for Restaurents
-    restaurent_mask = business_df['categories'].str.contains('Restaurants')
-    restaurent_df = business_df[restaurent_mask]
-
-    # Creating mask for Nightlife
-    nightlife_mask = business_df['categories'].str.contains('Nightlife')
-    nightlife_df = business_df[nightlife_mask]
-
-    global hotel_state_df_map
-    global restaurent_state_df_map
-    global nightlife_state_df_map
-
-    for state in top_states:
-        df_name = f'business_df_{state}'
-
-        hotel_state_df = hotel_df[hotel_df['state'] == state]
-        restaurent_state_df = restaurent_df[restaurent_df['state'] == state]
-        nightlife_state_df = nightlife_df[nightlife_df['state'] == state]
-
-        exec(f"{df_name} = hotel_state_df")
-        # add the dataframe to the dictionary with the state abbreviation as the key
-        hotel_state_df_map[state] = hotel_state_df
-
-        exec(f"{df_name} = restaurent_state_df")
-        # add the dataframe to the dictionary with the state abbreviation as the key
-        restaurent_state_df_map[state] = restaurent_state_df
-
-        exec(f"{df_name} = nightlife_state_df")
-        # add the dataframe to the dictionary with the state abbreviation as the key
-        nightlife_state_df_map[state] = nightlife_state_df
-
-
-def compressed_pickle(title, data):
-    with bz2.BZ2File(title + ".pbz2", "w") as f:
-        pickle.dump(data, f)
-
-
-def decompress_pickle(file):
-    data = bz2.BZ2File(file, "rb")
-    data = pickle.load(data)
-    return data
-
+with app.app_context():
+    helper_fun()
 
 # loadCSV()
 # getTopStates()
@@ -613,9 +618,9 @@ def getNPRRecommendation(rec_type, state_name, user_id):
     global nightlife_state_rec_map
     print("HOTEL MAP SIZE:")
     print(len(hotel_state_rec_map))
-    if (len(hotel_state_rec_map) == 0 or len(restaurent_state_rec_map) == 0 or 
-        len(nightlife_state_rec_map) == 0):
-        helper_fun()
+    # if (len(hotel_state_rec_map) == 0 or len(restaurent_state_rec_map) == 0 or
+    #         len(nightlife_state_rec_map) == 0):
+    #     helper_fun()
 
     if rec_type == "hotel":
         recommendations_class = hotel_state_rec_map[state_name]
@@ -638,11 +643,11 @@ def getMFRecommendation(rec_type, state_name, user_id):
     global hotel_state_rec_map
     global restaurent_state_rec_map
     global nightlife_state_rec_map
-    if (len(hotel_mf_map) or len(restaurent_mf_map) or 
-        len(nightlife_mf_map) or len(hotel_state_rec_map) == 0 or 
-        len(restaurent_state_rec_map) == 0 or 
-        len(nightlife_state_rec_map) == 0):
-        helper_fun()
+    # if (len(hotel_mf_map) or len(restaurent_mf_map) or
+    #     len(nightlife_mf_map) or len(hotel_state_rec_map) == 0 or
+    #     len(restaurent_state_rec_map) == 0 or
+    #         len(nightlife_state_rec_map) == 0):
+    #     helper_fun()
 
     if rec_type == "hotel":
         mf_recommendations = hotel_mf_map[state_name]
@@ -677,11 +682,11 @@ def getAECFRecommendation(rec_type, state_name, user_id):
     global restaurent_state_rec_map
     global nightlife_state_rec_map
 
-    if (len(hotel_aecf_map) or len(restaurent_aecf_map) or 
-        len(nightlife_aecf_map) or len(hotel_state_rec_map) == 0 or 
-        len(restaurent_state_rec_map) == 0 or 
-        len(nightlife_state_rec_map) == 0):
-        helper_fun()
+    # if (len(hotel_aecf_map) or len(restaurent_aecf_map) or
+    #     len(nightlife_aecf_map) or len(hotel_state_rec_map) == 0 or
+    #     len(restaurent_state_rec_map) == 0 or
+    #         len(nightlife_state_rec_map) == 0):
+    #     helper_fun()
 
     if rec_type == "hotel":
         aecf_recommendations = hotel_aecf_map[state_name]
@@ -1038,6 +1043,6 @@ if __name__ == '__main__':
     # print("Loading Nightlife AECF Files Completed...")
     # print(datetime.datetime.now())
     # print("\n")
-    helper_fun()
+    # helper_fun()
     app.run(debug=False)
-    # app.run(port = 8080, host = '0.0.0.0', debug=False)
+    # app.run(port=8080, host='0.0.0.0', debug=False)
